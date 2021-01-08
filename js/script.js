7ec131d05e3a6cae12897253b78665971f0830e9
marco sartori <marcosartori98@yahoo.com>
2021-01-08 15:14:46 +0100

    scorrimento pallino next

--- js/script.js
+++ js/script.js
@@ -12,14 +12,21 @@ $(document).ready(function(){
 
 // FUNZIONE PULSANTE NEXT
 function pulsanteNext(){
+  //SCORRIMENTO IMMAGINI
   var imgAttiva = $('.images img.active');
-
   imgAttiva.removeClass('active');
   imgAttiva.next().addClass('active');
+
+  //SCORRIMENTO PALLINI
+  var pallino = $('.nav i.active');
+  pallino.removeClass('active');
+  pallino.next().addClass('active');
+
 
 
   if(imgAttiva.hasClass('last')){
     $('.images img.first').addClass('active');
+    $('.nav i.first').addClass('active');
   }
 
 }
