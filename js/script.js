22c2f059cae2bbfd07edbb206a84da4f66f33840
marco sartori <marcosartori98@yahoo.com>
2021-01-08 15:11:16 +0100

    scorrimento immagini next

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 
+  //AL CLICK RICHIAMO LA FUNZIONE
+  $('.next').click(pulsanteNext);
+
 });
+
+
+
+
+
+
+// FUNZIONE PULSANTE NEXT
+function pulsanteNext(){
+  var imgAttiva = $('.images img.active');
+
+  imgAttiva.removeClass('active');
+  imgAttiva.next().addClass('active');
+
+
+  if(imgAttiva.hasClass('last')){
+    $('.images img.first').addClass('active');
+  }
+
+}
